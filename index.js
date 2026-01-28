ae58ac01fca0fe5dc2ec9ed70fbe7b88ee3bc1db
ibratkamulov <ibratkamulov@gmail.com>
2026-01-28 10:55:02 +0500

    4.5-vazifa

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ const app = http.createServer((req, res) => {
     const reqID = Number(req.url.split("/")[2])
 
 
-    //////////////////products/////////////////////////////////
+    //////////////////student/////////////////////////////////
     ///GET
-    if (req.method === "GET" && req.url === "/products") {
-        const products = read_file("products.json")
+    if (req.method === "GET" && req.url === "/students") {
+        const students = read_file("students.json")
         res.writeHead(200, options)
-        res.end(JSON.stringify(products))
+        res.end(JSON.stringify(students))
     }
 
     ///POST
-    if (req.method === "POST" && req.url === "/add_product") {
+    if (req.method === "POST" && req.url === "/add_student") {
         req.on("data", (chunk) => {
             const data = JSON.parse(chunk)
-            const products = read_file("products.json")
-            products.push({
+            const students = read_file("students.json")
+            students.push({
                 ...data
             })
-            write_file("products.json", products)
+            write_file("students.json", students)
             res.writeHead(201, options)
             res.end(JSON.stringify({
-                message: "add new product"
+                message: "add new student"
             }))
         })
     }
@@ -34,14 +34,14 @@ const app = http.createServer((req, res) => {
 
     ////GET ONE
     if (req.method === "GET" && req.url === `/get_one/${reqID}`) {
-        const products = read_file("products.json")
-        const findOne = products.find((item) => item.id === reqID)
+        const students = read_file("students.json")
+        const findOne = students.find((item) => item.id === reqID)
         if (findOne) {
             res.writeHead(200, options);
             res.end(JSON.stringify(findOne));
         } else {
             res.writeHead(404, options);
-            res.end(JSON.stringify({ message: "Product not found" }));
+            res.end(JSON.stringify({ message: "student not found" }));
         }
     }
 
@@ -51,15 +51,15 @@ const app = http.createServer((req, res) => {
         req.on("data", (chunk) => {
             const data = JSON.parse(chunk)
             const { title, desc, img, price } = data
-            const products = read_file("products.json")
-            const findOne = products.find((item) => item.id === reqID)
+            const students = read_file("students.json")
+            const findOne = students.find((item) => item.id === reqID)
             if (!findOne) {
                 res.writeHead(404, options);
                 return res.end(JSON.stringify({
-                    message: "Product not found"
+                    message: "Student not found"
                 }));
             }
-            products.forEach((item) => {
+            students.forEach((item) => {
                 if (item.id === reqID) {
                     item.title = title ? title : item.title
                     item.desc = desc ? desc : item.desc
@@ -67,10 +67,10 @@ const app = http.createServer((req, res) => {
                     item.img = img ? img : item.img
                 }
             })
-            write_file("products.json", products)
+            write_file("students.json", students)
             res.writeHead(201, options)
             res.end(JSON.stringify({
-                message: "product update"
+                message: "student update"
             }))
         })
     }
@@ -78,78 +78,78 @@ const app = http.createServer((req, res) => {
 
     //DELETE
     if (req.method === "DELETE" && req.url === `/delete/${reqID}`) {
-        const products = read_file("products.json")
-        const findOne = products.find((item) => item.id === reqID)
+        const students = read_file("students.json")
+        const findOne = students.find((item) => item.id === reqID)
         if (!findOne) {
             res.writeHead(404, options);
             return res.end(JSON.stringify({
-                message: "Product not found"
+                message: "Student not found"
             }));
         }
-        products.forEach((item, idx) =>{
+        students.forEach((item, idx) =>{
             if(item.id===reqID){
-              products.splice(idx, 1)
+              students.splice(idx, 1)
             }
         })
-        write_file("products.json", products)
+        write_file("students.json", students)
         res.writeHead(201, options)
         res.end(JSON.stringify({
-            message: "product deleted"
+            message: "student deleted"
         }))
     }
 
 
-    ///////////////animals//////////////////////////////////////////////
+    ///////////////hospital//////////////////////////////////////////////
     ///GET
-if (req.method === "GET" && req.url === "/animals") {
-    const animals = read_file("animals.json")
+if (req.method === "GET" && req.url === "/hospitals") {
+    const hospitals = read_file("hospitals.json")
     res.writeHead(200, options)
-    res.end(JSON.stringify(animals))
+    res.end(JSON.stringify(hospitals))
 }
 
 ///POST
-if (req.method === "POST" && req.url === "/add_animal") {
+if (req.method === "POST" && req.url === "/add_hospital") {
     req.on("data", (chunk) => {
         const data = JSON.parse(chunk)
-        const animals = read_file("animals.json")
-        animals.push({
+        const hospitals = read_file("hospitals.json")
+        hospitals.push({
             ...data
         })
-        write_file("animals.json", animals)
+        write_file("hospitals.json", hospitals)
         res.writeHead(201, options)
         res.end(JSON.stringify({
-            message: "add new animal"
+            message: "add new hospital"
         }))
     })
 }
 
 ///GET ONE
-if (req.method === "GET" && req.url === `/get_one_animal/${reqID}`) {
-    const animals = read_file("animals.json")
-    const findOne = animals.find((item) => item.id === reqID)
+if (req.method === "GET" && req.url === `/get_one_hospital/${reqID}`) {
+    const hospitals = read_file("hospitals.json")
+    const findOne = hospitals.find((item) => item.id === reqID)
     if (findOne) {
         res.writeHead(200, options);
         res.end(JSON.stringify(findOne));
     } else {
         res.writeHead(404, options);
-        res.end(JSON.stringify({ message: "Animal not found" }));
+        res.end(JSON.stringify({ message: "Hospital not found" }));
     }
 }
 
 ///PUT
-if (req.method === "PUT" && req.url === `/update_animal/${reqID}`) {
+if (req.method === "PUT" && req.url === `/update_hospital/${reqID}`) {
     req.on("data", (chunk) => {
         const data = JSON.parse(chunk)
         const { name, type, age, image } = data
-        const animals = read_file("animals.json")
-        const findOne = animals.find((item) => item.id === reqID)
+        const hospitals = read_file("hospitals.json")
+        const findOne = hospitals.find((item) => item.id === reqID)
         if (!findOne) {
             res.writeHead(404, options);
             return res.end(JSON.stringify({
-                message: "Animal not found"
+                message: "Hospital not found"
             }));
         }
-        animals.forEach((item) => {
+        hospitals.forEach((item) => {
             if (item.id === reqID) {
                 item.name = name ? name : item.name
                 item.type = type ? type : item.type
@@ -157,123 +157,35 @@ if (req.method === "PUT" && req.url === `/update_animal/${reqID}`) {
                 item.image = image ? image : item.image
             }
         })
-        write_file("animals.json", animals)
+        write_file("hospitals.json", hospitals)
         res.writeHead(201, options)
         res.end(JSON.stringify({
-            message: "animal updated"
+            message: "hospital updated"
         }))
     })
 }
 
 ///DELETE
-if (req.method === "DELETE" && req.url === `/delete_animal/${reqID}`) {
-    const animals = read_file("animals.json")
-    const findOne = animals.find((item) => item.id === reqID)
+if (req.method === "DELETE" && req.url === `/delete_hospital/${reqID}`) {
+    const hospitals = read_file("hospitals.json")
+    const findOne = hospitals.find((item) => item.id === reqID)
     if (!findOne) {
         res.writeHead(404, options);
         return res.end(JSON.stringify({
-            message: "Animal not found"
+            message: "Hospital not found"
         }));
     }
-    animals.forEach((item, idx) => {
+    hospitals.forEach((item, idx) => {
         if (item.id === reqID) {
-            animals.splice(idx, 1)
+            hospitals.splice(idx, 1)
         }
     })
-    write_file("animals.json", animals)
+    write_file("hospitals.json", hospitals)
     res.writeHead(201, options)
     res.end(JSON.stringify({
-        message: "animal deleted"
+        message: "hospital deleted"
     }))
 }
-
-
-//////////////fruit////////////////////////////
-/// GET
-if (req.method === "GET" && req.url === "/fruits") {
-    const fruits = read_file("fruits.json");
-    res.writeHead(200, options);
-    res.end(JSON.stringify(fruits));
-}
-
-/// POST
-if (req.method === "POST" && req.url === "/add_fruit") {
-    req.on("data", (chunk) => {
-        const data = JSON.parse(chunk);
-        const fruits = read_file("fruits.json");
-        fruits.push({ ...data });
-        write_file("fruits.json", fruits);
-        res.writeHead(201, options);
-        res.end(JSON.stringify({
-            message: "add new fruit"
-        }));
-    });
-}
-
-/// GET ONE
-if (req.method === "GET" && req.url === `/get_one_fruit/${reqID}`) {
-    const fruits = read_file("fruits.json");
-    const findOne = fruits.find((item) => item.id === reqID);
-    if (findOne) {
-        res.writeHead(200, options);
-        res.end(JSON.stringify(findOne));
-    } else {
-        res.writeHead(404, options);
-        res.end(JSON.stringify({ message: "Fruit not found" }));
-    }
-}
-
-/// PUT
-if (req.method === "PUT" && req.url === `/update_fruit/${reqID}`) {
-    req.on("data", (chunk) => {
-        const data = JSON.parse(chunk);
-        const { name, type, age, image } = data;
-        const fruits = read_file("fruits.json");
-        const findOne = fruits.find((item) => item.id === reqID);
-        if (!findOne) {
-            res.writeHead(404, options);
-            return res.end(JSON.stringify({
-                message: "Fruit not found"
-            }));
-        }
-        fruits.forEach((item) => {
-            if (item.id === reqID) {
-                item.name = name ? name : item.name;
-                item.type = type ? type : item.type;
-                item.age = age ? age : item.age;
-                item.image = image ? image : item.image;
-            }
-        });
-        write_file("fruits.json", fruits);
-        res.writeHead(201, options);
-        res.end(JSON.stringify({
-            message: "fruit updated"
-        }));
-    });
-}
-
-/// DELETE
-if (req.method === "DELETE" && req.url === `/delete_fruit/${reqID}`) {
-    const fruits = read_file("fruits.json");
-    const findOne = fruits.find((item) => item.id === reqID);
-    if (!findOne) {
-        res.writeHead(404, options);
-        return res.end(JSON.stringify({
-            message: "Fruit not found"
-        }));
-    }
-    fruits.forEach((item, idx) => {
-        if (item.id === reqID) {
-            fruits.splice(idx, 1);
-        }
-    });
-    write_file("fruits.json", fruits);
-    res.writeHead(201, options);
-    res.end(JSON.stringify({
-        message: "fruit deleted"
-    }));
-}
-
 
 
 })
